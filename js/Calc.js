9162596264b861a122f18f7c274c630168906d97
Murilo T. <mr.murilo148@gmail.com>
2026-06-08 21:05:28 -0300

    Projetinho...Calc

--- js/Calc.js
+++ js/Calc.js
@@ -1,11 +1,36 @@
+const botao = document.querySelector("#btnCalcular");
+const numero1 = document.querySelector("#num1");
+const numero2 = document.querySelector("#num2");
+const resultado = document.querySelector("#resultado");
+
 function somar() {
-    let numero1, numero2, resultado;
+    const num1 = Number(numero1.value);
+    const num2 = Number(numero2.value);
 
-    numero1 = Number(document.querySelector('#num1').value);
-    numero2 = Number(document.querySelector('#num2').value);
-    resultado = document.querySelector('#resultado').innerHTML;
+    const resCalc = num1 + num2;
 
-    resultado = numero1 + numero2
+    resultado.textContent = resCalc;
 
-    document.querySelector('#resultado').innerHTML = resultado;
+    console.log(isNaN(num1));
+    console.log(isNaN(num1));
+}
+
+function validarEntradas() {
+    if (isNaN(numero1.value) === "" || isNaN(numero2.value) === "") {
+        return false
+        // Não posso calcular
+    } else {
+        return true
+        // Posso calcular
+    }
+
+}
+
+function mostrarMensagemErro() {
+    resultado.innerHTML = "Valores inválidos. Insira novos valores (números)"
+}
+
+
+if (validarEntradas() === true) {
+    botao.addEventListener("click", somar);
 }
